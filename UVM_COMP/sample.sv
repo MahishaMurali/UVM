c59a24ada6459402bd7c2ad20cb703d8996d0a51
MahishaMurali <mahishaballapura@ufl.edu>
2025-03-02 08:47:12 -0500

    Create sample.sv

---
+++ UVM_COMP/sample.sv
@@ -0,0 +1,18 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*; 
+class sample extends uvm_component;
+`uvm_component_utils(sample)
+  function new(string path = "sample", uvm_component parent= null );
+super.new(path,parent);
+endfunction
+virtual function void build_phase(uvm_phase phase);
+super.build_phase(phase);
+`uvm_info("comp","build_phase", UVM_NONE);
+endfunction
+endclass
+module tb;
+
+initial begin
+run_test("sample");
+end
+endmodule
